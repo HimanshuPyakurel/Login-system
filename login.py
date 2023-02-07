0b3aeb9ec46bf30fcb884cd8bdd24cc41f3a891c
HimanshuPyakurel <himansupyakurel89@gmail.com>
2023-02-07 20:49:33 +0545

    Second Commit

--- login.py
+++ login.py
@@ -1,13 +1,14 @@
 from tkinter import *
 from PIL import ImageTk
 from tkinter import messagebox
+import pymysql
 
 #create the class
 class Login:
     def __init__(self,root):
         self.root = root
         self.root.title(" Login and Registration for Apps")
-        self.root.geometry("1366*700+0+0")
+        self.root.geometry("1366x700+0+0")
         self.root.resizable(False,False)
         self.loginform()
 
@@ -36,14 +37,31 @@ class Login:
         self.password = Entry(frame_input,font=("times new roman",15,"bold"),bg='lightgray')
         self.password.place(x=30,y=245,width=270,height=35)
 
-        btnl1 = Button(frame_input,text="forget password?",cursor='hand2',font=('calibri',10), fg ='black', bg='white',bg=0)
+        btnl1 = Button(frame_input,text="forget password?",cursor='hand2',font=('calibri',10), fg ='black', bg='white')
         btnl1.place(x=90,y=305)
 
-        btnl2 = Button(frame_input,text="Login",command =self.login, cursor='hand2',font=('times new roman',15), fg ='white', bg='orangered',bg=0,width=15,height=1)
+        btnl2 = Button(frame_input,text="Login",command =self.login, cursor='hand2',font=('times new roman',15), fg ='white', bg='orangered',width=15,height=1)
         btnl2.place(x=90,y=340) 
         
-        btnl3 = Button(frame_input,text="Not Registered?register",command=self.register,cursor='hand2',font=('calibri',10), fg ='black', bg='white',bg=0)
+        btnl3 = Button(frame_input,text="Not Registered?register",command=self.register,cursor='hand2',font=('calibri',10), fg ='black', bg='white')
         btnl3.place(x=110,y=390)
+
+    def login(self):
+        if self.email_txt.get()=="" or self.password.get()=="":
+            messagebox.showerror("Error","All fields are required",parent=self.root)
+        else:
+            try:
+                con=pymysql.connect(host='localhost',user='root',password='Hp521678', database='student')
+                cur = con.cursor()
+                cur.execute('select * from register where email=%s and password=%s',(self.email_try.get(),self.password.get()))
+                row=cur.fetchone()
+                if row == None:
+                    messagebox.showerror('Error','Invalid Username And Password',parent=self.root)
+                else:
+                    self.appscreen()
+                    con.close()
+            except Exception as es:
+                messagebox.showerror('Error',f'Error Due to : {str(es)}',parent=self.root)
 
 root =Tk()
 ob=Login(root)
